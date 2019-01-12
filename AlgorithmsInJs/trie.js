db492db475ee35662547d9d5852efb860fefc1a9
Shalindra Shrestha <shalindra.shrestha@gmail.com>
2019-01-12 13:32:24 -0800

    Update trie.js

--- AlgorithmsInJs/trie.js
+++ AlgorithmsInJs/trie.js
@@ -1,26 +1,23 @@
-
 //https://leetcode.com/problems/add-and-search-word-data-structure-design/description/
 // and print all the words in trie
-
-
-class Node{
-    constructor(){
+class Node {
+    constructor() {
         this.children = new Map(); // add children as map. key is the alphabet and values are the children linked to that alphabet
         this.isEnd = false; //if true, that ending is a word
     }
 }
 
 
-class WordDictionary{
-    constructor(){
+class WordDictionary {
+    constructor() {
         this.root = new Node();
     }
 
-    addWord(word){
+    addWord(word) {
         let current = this.root;
 
-        for(let i =0; i< word.length; i++){
-            if(!current.children.has(word[i])){ // if doesnt have char as children, create one. If it does, ignore it. this way we avoid duplicate
+        for (let i = 0; i < word.length; i++) {
+            if (!current.children.has(word[i])) { // if doesnt have char as children, create one. If it does, ignore it. this way we avoid duplicate
                 current.children.set(word[i], new Node()) //each new addition is a new Node() 
             }
             current = current.children.get(word[i])
@@ -28,20 +25,20 @@ class WordDictionary{
         current.isEnd = true;
     }
 
-    search(word){
-        function searchWord(curr, idx){
-            if(!curr || (word.length == idx && !curr.isEnd)){
+    search(word) {
+        function searchWord(curr, idx) {
+            if (!curr || (word.length == idx && !curr.isEnd)) {
                 return false
             }
-            if(word.length == idx && curr.isEnd){
+            if (word.length == idx && curr.isEnd) {
                 return true
             }
-            if(word[idx] == '.'){ // if '.' then search for each character and see if exists in the children, if it does the do recursion
+            if (word[idx] == '.') { // if '.' then search for each character and see if exists in the children, if it does the do recursion
                 let begin = 'a'.charCodeAt();
-                for(let i = 0; i < 26; i++){
+                for (let i = 0; i < 26; i++) {
                     let char = String.fromCharCode(begin + i);
-                    if(curr.children.has(char)){
-                        if(searchWord(curr.children.get(char), idx + 1)){
+                    if (curr.children.has(char)) {
+                        if (searchWord(curr.children.get(char), idx + 1)) {
                             return true
                         }
                     }
@@ -53,18 +50,18 @@ class WordDictionary{
         return searchWord(this.root, 0)
     }
 
-    printAll(){
+    printAll() {
         let words = []
-        let print = function(current, str){
-            if(current.children.size){ // children exists then recurse
+        let print = function(current, str) {
+            if (current.children.size) { // children exists then recurse
                 for (let letter of current.children.keys()) {
-					print(current.children.get(letter), str.concat(letter));
-				};
-				if (current.isEnd()) {
-					words.push(str);
-				};
+                    print(current.children.get(letter), str.concat(letter));
+                };
+                if (current.isEnd()) {
+                    words.push(str);
+                };
             } else { //if no children and str exist then that is end of trie and push it to list of words
-                if(str.length){
+                if (str.length) {
                     words.push(str)
                 }
                 return
