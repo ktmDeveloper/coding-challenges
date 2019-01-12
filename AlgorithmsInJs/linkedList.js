23dd54959753047ebe5d9e5264e05965d860ebb7
Shalindra Shrestha <shalindra.shrestha@gmail.com>
2019-01-12 12:07:59 -0800

    Update linkedList.js

--- AlgorithmsInJs/linkedList.js
+++ AlgorithmsInJs/linkedList.js
@@ -1,45 +1,77 @@
 class Node {
-    constructor(a) {
-        this.val = a, this.next = null
+    constructor(val) {
+        this.val = val
+        this.next = null
     }
 }
-class linkedL {
+
+class linkedList {
     constructor() {
-        this.head = null
+        this.root = null
+        this.length = 0
     }
-    add(a) {
-        let b = new Node(a);
-        if (this.head) {
-            let c = this.head;
-            for (; c.next;) c = c.next;
-            c.next = b
-        } else this.head = b
-    }
-    exist(a) {
-        if (!this.head) return !1;
-        for (let b = this.head; b;) {
-            if (b.val == a) return !0;
-            b = b.next
-        }
-        return !1
-    }
-    deleteVal(a) {
-        if (!this.head) return 'no value initated';
-        if (this.exist(a)) {
-            let c, b = this.head;
-            if (b.val == a) this.head = b.next;
-            else {
-                for (; b.val !== a;) c = b, b = b.next;
-                return c.next = b.next, b
+
+    add(val) {
+        if (this.root) {
+            let curr = this.root
+            while (curr.next) {
+                curr = curr.next
             }
-        } else return 'no value found to delete'
+            curr.next = new Node(val)
+            this.length++
+        } else {
+            this.root = new Node(val)
+            this.length++
+        }
+    }
+
+    exist(k) {
+        if (!this.root) return null
+
+        let curr = this.root
+        while (curr) {
+            if (curr.val == k) {
+                return true
+            }
+            curr = curr.next
+        }
+        return false
+    }
+
+    delete(k) {
+        if (!this.root) return null
+
+        let curr = this.root
+
+        if (curr.val == k) {
+            this.root = curr.next
+            this.length--
+            return curr
+        }
+
+        let prev = null
+        while (curr) {
+            if (curr.val == k) {
+                prev.next = curr.next
+                this.length--
+                return curr
+            }
+            prev = curr
+            curr = curr.next
+        }
+        return false
     }
 }
-let link = new linkedL;
-link.add(44) 
-link.add(443) 
-link.add(444)
-console.log(link)
- console.log(link.deleteVal(1))
- console.log(link.deleteVal(443)) 
- console.log(link);
+
+let ll = new linkedList()
+
+ll.add(1)
+ll.add(2)
+ll.add(3)
+ll.add(4)
+ll.add(40)
+
+console.log(ll)
+console.log(ll.delete(40))
+console.log(ll.delete(2))
+console.log(ll)
